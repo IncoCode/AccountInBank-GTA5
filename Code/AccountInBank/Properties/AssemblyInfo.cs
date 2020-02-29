550ee24646694b9ce044f24a7522d574c5f900c4
IncoCode <akulich.dimon@gmail.com>
2020-02-29 21:26:47 +0200

    Bump version

--- Code/AccountInBank/Properties/AssemblyInfo.cs
+++ Code/AccountInBank/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion( "2.0.0.0" )]
-[assembly: AssemblyFileVersion( "2.0.0.0" )]
+[assembly: AssemblyVersion( "2.0.1.0" )]
+[assembly: AssemblyFileVersion( "2.0.1.0" )]
